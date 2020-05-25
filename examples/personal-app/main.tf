59b4cae6a35efd1760b63447fec04174d6448e19
Henry Addison <henryaddison@users.noreply.github.com>
2020-05-25 20:52:37 +0100

    update examples for new interface for deploy hooks

--- examples/personal-app/main.tf
+++ examples/personal-app/main.tf
@@ -2,11 +2,17 @@ provider "heroku" {
     version = "~> 2.0"
 }
 
+locals {
+    environment = "test"
+    bugsnag_api_key = ""
+    repo = "https://github.com/henryaddison/my-rails-app"
+}
+
 module "heroku_app" {
     source  = "henryaddison/rails-app/heroku"
-    version = "0.0.3"
+    version = "0.0.4"
 
-    environment = "test"
+    environment = local.environment
     custom_domain = ""
     app_name_prefix = "tf-personal-example"
     region = "eu"
@@ -16,8 +22,10 @@ module "heroku_app" {
     db_plan = "hobby-dev"
     queue_redis_plan = ""
     cache_redis_plan = ""
-    bugsnag_api_key = ""
-    repo = "https://github.com/henryaddison/my-rails-app"
     heroku_team_name = ""
     papertrail_addon_name = ""
+
+    deploy_hook_urls = [
+        "https://build.bugsnag.com/heroku?apiKey=${local.bugsnag_api_key}&releaseStage=${local.environment}&repository=${urlencode(local.repo)}"
+    ]
 }
--- examples/team-app/main.tf
+++ examples/team-app/main.tf
@@ -2,9 +2,15 @@ provider "heroku" {
     version = "~> 2.0"
 }
 
+locals {
+    environment = "test"
+    bugsnag_api_key = ""
+    repo = "https://github.com/henryaddison/my-rails-app"
+}
+
 module "heroku_app" {
     source  = "henryaddison/rails-app/heroku"
-    version = "0.0.3"
+    version = "0.0.4"
 
     environment = "test"
     custom_domain = "terraform-team-example.hja22.co.uk"
@@ -16,8 +22,10 @@ module "heroku_app" {
     db_plan = "hobby-dev"
     queue_redis_plan = "hobby-dev"
     cache_redis_plan = "hobby-dev"
-    bugsnag_api_key = ""
-    repo = "https://github.com/henryaddison/my-rails-app"
     heroku_team_name = "hja22"
     papertrail_addon_name = ""
+
+    deploy_hook_urls = [
+        "https://build.bugsnag.com/heroku?apiKey=${local.bugsnag_api_key}&releaseStage=${local.environment}&repository=${urlencode(local.repo)}"
+    ]
 }
